0705767b5e0f1d00ab2b11d8d91af5b8d79dd574
Niklas Nikanti <niklas.nikanti@gmail.com>
2017-11-12 14:51:04 +0200

    port & ip

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const uploads = require('./routes/uploads');
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 
 // IP address
-const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
+const ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 
 // CORS Middleware
 app.use(cors());
